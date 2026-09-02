3d142e9569b4f17ad043788a373e0e6446f2d67e
jdrkl <164361926+jdrkl@users.noreply.github.com>
2026-09-02 15:03:27 +0100

    completed 06_repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(word, count) {
+    let repeatedString = "";
+    
+    if (count < 0) return "error";
 
+    for(let i = 0; i < count; i++) {
+        repeatedString += word;
+    }
+
+    return repeatedString;
 };
 
 // Do not edit below this line
