001297952742332f2441737f6d8f72c988adb622
jdrkl <164361926+jdrkl@users.noreply.github.com>
2026-09-03 11:09:40 +0100

    completed 10_leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    const divisibleByFour = year % 4 == 0;
+    const divisibleByFourHundred = year % 400 == 0;
+    const century = year % 100 == 0;
 
+    if ((divisibleByFour || divisibleByFourHundred) && !century) {
+        return true;
+    }
 };
 
 // Do not edit below this line
